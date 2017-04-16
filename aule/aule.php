d0c6760a3fa1d1e7dae03a01f89615308d76f0c4
Kojee <marco.filippozzi98@gmail.com>
2017-04-16 15:40:41 +0200

    Added list group to visualize every available classroom in the school

--- aule/aule.php
+++ aule/aule.php
@@ -1,8 +1,12 @@
 <?php
     session_start();
     
-    
-    
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "utenti";
+
+    $conn = new mysqli($servername, $username, $password, $dbname);   
 ?>
 <html>
     <head>
@@ -14,11 +18,36 @@
 
         <!-- Latest compiled and minified JavaScript -->
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-        <link rel="stylesheet" href="css/custom.css">
+        <link rel="stylesheet" href="../css/custom.css">
     </head>
     <body>
 
         <?php require "../snippets/navbar.php";?>
+        <div class="row">
+            <div class="col-md-4"></div>
+            <div class="col-md-4">
+                <div class="list-group">
+                    <?php
+                        if ($conn->connect_error) {
+                            die("Connection failed: " . mysqli_connect_error());
+                        }
+
+                        $stm = $conn->prepare("select nome, info from aule");
+
+                        if ($stm->execute()) {
+                            $result = $stm->get_result();
+                            while($row = $result->fetch_assoc()){
+                                echo    '<a href="#" class="list-group-item">
+                                        <h4 class="list-group-item-heading">' . $row["nome"] . '</h4>
+                                        <p class="list-group-item-text">' . $row["info"] . '</p>
+                                        </a>';
+                            }
+                        }
+                    ?>
+                </div>         
+            </div>
+            <div class="col-md-4"></div>
+        </div>
         
     </body>
 </html>
